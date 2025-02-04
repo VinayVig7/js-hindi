a8b40dd96d881b844a425a7022e254c3b808d6bb
Vinay <97362344+VinayVig7@users.noreply.github.com>
2025-02-04 11:20:09 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);  //Js will convert "2" in number
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null <= 0);
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+// === strict   this will check data type also
+
+console.log("2" === 2);
+
+
