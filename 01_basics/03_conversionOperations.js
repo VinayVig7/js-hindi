91ccd34ab1d701de8feb787b47ba38f92e222889
Vinay <97362344+VinayVig7@users.noreply.github.com>
2025-02-04 07:55:48 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,27 @@
+let score = "Vinay"
+console.log(typeof (score));
+console.log(score);
+
+let valueInNumber = Number(score);
+console.log(valueInNumber);
+console.log(typeof(valueInNumber));
+
+// "33"=> 33
+//  "33abc"=> NaN
+// true=> 1 , false=> 0
+
+let isLoggedIn = "Vinay";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1=> true 0=> false
+//""=> false
+//"Vinay"=> true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
