6b29b12274ccf67fc6e5e5179921699497fc6b83
Vinay <97362344+VinayVig7@users.noreply.github.com>
2025-02-04 11:03:33 +0000

    operations plus conversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -24,6 +24,41 @@ let stringNumber = String(someNumber);
 
 console.log(stringNumber);
 console.log(typeof(stringNumber));
-//push
+
+// **************Operations************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Vinay";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");  this type of practice is not good code readability should be priority
+
+// console.log(+true);
+// console.log(+""); // this type of practice is not good code readability should be priority
+
+// learn prefix and postfix in mdn.
+
+
+
+
+
+
+
 
 
